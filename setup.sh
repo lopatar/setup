5d3c66474780f9e2e36bddbc6eb3ccaf30c06e56
dpo9ScYw <dpo9ScYw@protonmail.com>
2021-10-27 09:30:07 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,23 @@
+apt update
+apt upgrade -y
+
+user=$(whoami)
+cp files/start.sh /home/$user/start.sh
+
+snap install keepassxc
+snap install codium
+
+apt install -y wget apt-transport-https
+
+get -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
+‍echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+apt update
+apt install element-desktop
+apt install signal-desktop
+apt install macchanger
+
