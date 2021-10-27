ee9b68a2c9329139be199ca3ed707265961e3299
dpo9ScYw <dpo9ScYw@protonmail.com>
2021-10-27 10:50:28 +0200

    update

--- files/start.sh
+++ files/start.sh
@@ -1,15 +1,15 @@
 #Turn off the network interface
-ifconfig wlp3s0 down
+sudo ifconfig wlp3s0 down
 #Change the interface's MAC to a random value
-macchanger -r wlp3s0
+sudo macchanger -r wlp3s0
 #Turn on the network interface
-ifconfig wlp3s0 up
+sudo ifconfig wlp3s0 up
 #Connect to wireguard server
-wg quick up contabo
+sudo wg quick up contabo
 #Clear output
 echo Connect to the internet and press any key, system will be updated afterwards.
 #Wait for input
 read dummy
 #Update the system
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
--- setup.sh
+++ setup.sh
@@ -1,42 +1,44 @@
 # Update our system and repos
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 
-#Copy the starting script to our home directory
+#Copy the start script and keepass db into corresponding folders
 cp files/start.sh ~/start.sh
+cp files/KeePass_DB.kdbx ~/Documents/KeePass_DB.kdbx
 
-#enable snap
+#Enable snap
 mv /etc/apt/preferences.d/nosnap.pref ~
 
 #Install snap
-apt install -y snapd
+sudo apt install -y snapd
 
-#Install keepass and vscodium
-snap install keepassxc
-snap install codium
+#Install keepass, vscodium and netbeans 
+sudo snap install keepassxc
+sudo snap install codium
+sudo snap install netbeans --classic
 
 #Install wget
-apt install -y wget apt-transport-https
+sudo apt install -y wget apt-transport-https
 
-#Add signal, element and microsoft repositories
-get -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
+#Add signal, element, veracrypt and microsoft repositories
+sudo add-apt-repository ppa:unit193/encryption
+sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
 ‍echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
 cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
   tee -a /etc/apt/sources.list.d/signal-xenial.list
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 #Update repos
-apt update
+sudo apt update
 
-#Install element, signal and macchanger and .NET SDK
-apt install -y element-desktop
-apt install -y signal-desktop
-apt install -y macchanger
-apt install -y dotnet-sdk-5.0
-
-
+#Install element, signal, veracrypt and macchanger, .NET SDK and Veracrypt
+sudo apt install -y element-desktop
+sudo apt install -y signal-desktop
+sudo apt install -y macchanger
+sudo apt install -y dotnet-sdk-5.0
+sudo apt install -y veracrypt
 
